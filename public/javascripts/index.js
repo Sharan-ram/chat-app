47d4fd0d12e4775cbaa141ce37a9134e89400697
Sharan Ram <sharanram1993@gmail.com>
2017-06-09 09:15:11 +0530

    add multiple rooms with switch room function

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,8 +1,38 @@
 var socket = io();
-var button = document.getElementById("send");
-button.onclick = () => {
-  let message = document.getElementById("typing-box");
-  socket.emit("chat message", message.value);
-  message.value = "";
-  return false;
-};
+socket.on("connect", function() {
+  socket.emit("adduser", prompt("What's your name?"));
+});
+
+socket.on("updatechat", function(username, data) {
+  $("#conversation").append("<b>" + username + ":</b> " + data + "<br>");
+});
+
+socket.on("updaterooms", function(rooms, current_room) {
+  $("#rooms").empty();
+  $.each(rooms, function(key, value) {
+    if (value == current_room) {
+      $("#rooms").append("<div>" + value + "</div>");
+    } else {
+      $("#rooms").append(
+        '<div><a href="#" onclick="switchRoom(\'' +
+          value +
+          "')\">" +
+          value +
+          "</a></div>"
+      );
+    }
+  });
+});
+
+function switchRoom(room) {
+  socket.emit("switchRoom", room);
+}
+
+$(function() {
+  $("#datasend").click(function() {
+    var message = $("#data").val();
+    $("#data").val("");
+
+    socket.emit("sendchat", message);
+  });
+});
